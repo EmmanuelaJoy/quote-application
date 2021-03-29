e4d61836f15522dbec2597b8a1a5a8610d02f465
Emmanuela Joy <emmajoy81@gmail.com>
2021-03-29 16:08:01 +0300

    add styling changes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.backgroundColor = 'pink';
+    this.elem.nativeElement.style.backgroundColor = 'lightskyblue';
   }
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,14 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote('Everything will be okay in the end if it is not okay, it is not the end.', 'John Lennon', 'Emmanuela', new Date(2021, 2, 12)),
-    new Quote('I am no longer accepting the things i cannot change, i am changing the things i cannot accept.', 'Angela Davis', 'Emmanuela', new Date(2021, 1, 5)),
-    new Quote('Do not live the same year 75 times and call it a life.', 'R Sharma', 'Emmanuela', new Date(2020, 11, 8)),
-    new Quote('Suffer the pain of discipline or suffer the pain of regret.', 'John Lennon', 'Emmanuela', new Date(2020, 5, 10)),
-    new Quote('Everything will be okay in the end if it is not okay, it is not the end.', 'John Lennon', 'Emmanuela', new Date(2019, 10, 20)),
-    new Quote('Everything will be okay in the end if it is not okay, it is not the end.', 'John Lennon', 'Emmanuela', new Date(2019, 2, 7)),
-    new Quote('Everything will be okay in the end if it is not okay, it is not the end.', 'John Lennon', 'Emmanuela', new Date(2018, 9, 15)),
-    new Quote('There is a danger in letting people misname you. If you are a fire, do not answer when they call you a spark.', 'Upile Chisala', 'Emmanuela', new Date(2018, 4, 14))
+    new Quote('Be sure you put your feet in the right place, then stand firm.', 'Abraham Lincoln', 'Emmanuela', new Date(2021, 3, 15)),
+    new Quote('And if it does nothing else for you, it should make you happy.', 'Bob Ross', 'Emmanuela', new Date(2021, 4, 5)),
+    new Quote('There are no limits to what you can accomplish, except the limits you place on your own thinking.', 'Brian Tracy', 'Emmanuela', new Date(2021, 6, 12)),
+    new Quote('What a wonderful thought it is that some of the best days in our lives have not even happened yet.', 'Anne Frank', 'Emmanuela', new Date(2020, 11, 8)),
+    new Quote('We must all suffer one of two things: the pain of discipline or the pain of regret.', 'Jim Rohn', 'Emmanuela', new Date(2020, 10, 10)),
+    new Quote('Everything will be okay in the end if it is not okay, it is not the end.', 'John Lennon', 'Emmanuela', new Date(2020, 8, 20)),
+    new Quote('You don’t get results by focusing on results. You get results by focusing on the actions that produce results.', 'Mike Hawkins', 'Emmanuela', new Date(2019, 11, 20)),
+    new Quote('There is a danger in letting people misname you. If you are a fire, do not answer when they call you a spark.', 'Upile Chisala', 'Emmanuela', new Date(2019, 7, 14))
   ];
 
   addNewQuote(quote) {
